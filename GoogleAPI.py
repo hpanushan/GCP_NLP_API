5713226f8bf5ebcdb596b716657f5cd4ef00184e
anushanramesh <anushan@globalwavenet.com>
2019-07-10 15:38:54 +0530

    Normal script is converted into a function

--- GoogleAPI.py
+++ GoogleAPI.py
@@ -2,19 +2,20 @@ from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
 
-# Create a client to access GCP
-client = language.LanguageServiceClient()
+def gcpNLP(text):
+    # Create a client to access GCP
+    client = language.LanguageServiceClient()
 
-text = 'Now we know how to assemple the system'
+    # Testing text(Ignore the language)
+    document = language.types.Document(content=text,type='PLAIN_TEXT',)
 
-# Testing text(Ignore the language)
-document = language.types.Document(content=text,type='PLAIN_TEXT',)
+    # Getting the response for sentiment analysis
+    response = client.analyze_sentiment(document=document,encoding_type='UTF32',)
 
-# Getting the response for sentiment analysis
-response = client.analyze_sentiment(document=document,encoding_type='UTF32',)
+    # Getting respective sentment values
+    sentiment = response.document_sentiment
 
-# Getting respective sentment values
-sentiment = response.document_sentiment
+    return sentiment.score
 
-print(sentiment.score)
+
 
