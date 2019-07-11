81900db2c9ac8aba7bedd196cf95ca21a6cb2c32
anushanramesh <anushan@globalwavenet.com>
2019-07-11 13:23:05 +0530

    StreamClass is connected with main method

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from GoogleAPI import gcpNLP
-from Streaming import StdOutListener
+from Streaming import StreamingClass
 
 def main():
-    obj = StdOutListener()                          # Creating object of Tweeter streaming class
-    keywords = ["obama"]                            # Keyword list to filter tweets from streaming
-    obj.streamer(keywords)                          # Stream the tweets
+    obj = StreamingClass("Obama",10)                          # Creating object of Tweeter streaming class
+    print(obj.getTweetText())
 
 if __name__ == "__main__": 
     # calling main function 
