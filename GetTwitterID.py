26e285559620ccae6c0cd2751f5df665c6fecf5e
anushanramesh <anushan@globalwavenet.com>
2019-07-22 13:03:58 +0530

    Modified the getTwitterID script to get twitterID list

--- GetTwitterID.py
+++ GetTwitterID.py
@@ -5,5 +5,8 @@ def getTwitterID(screenName):
     t = twitter.Twitter(auth=twitter.OAuth(Credentials.ACCESS_TOKEN, Credentials.ACCESS_TOKEN_SECRET, Credentials.CONSUMER_KEY, Credentials.CONSUMER_KEY_SECRET))
     response = t.users.lookup(screen_name=screenName)
     responseList = list(response)
+    userIDList = []
+    userIDList.append(str(responseList[0]['id']))
 
-    return responseList[0]['id']
+    return userIDList
+
